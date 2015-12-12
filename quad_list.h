4209fc71ea0a424df4fb02614cc1c8d1224ed878
UltimateNinja <bloxz@hotmail.fr>
2015-12-12 20:39:22 +0100

    ajout struct

--- quad_list.h
+++ quad_list.h
@@ -5,7 +5,7 @@
 struct quad_list* quad_list_new(struct quad*)
 void quad_list_free(struct quad_list*)
 void quad_list_add(struct quad_list**, struct quad_list*)
-void quad_list_complete(struct quad_list*,struct_symbol*)
+void quad_list_complete(struct quad_list*,struct struct_symbol*)
 void quad_list_print(struct quad_list*)
 
 #endif
